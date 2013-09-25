655e3efa8d98a34ca0efbd7cc48b99140678916f
chanlevan <chanlevan@dbe85ffb-98f7-9858-1d73-24c5edbc5cac>
2013-09-25 12:15:42 +0000

    update code

--- src/main/java/org/deri/cqels/engine/windows/LinkedStreamItem.java
+++
@@ -1,5 +0,0 @@
-package org.deri.cqels.engine.windows;
-
-public abstract class LinkedStreamItem extends StreamItem {
-	 public LinkedStreamItem next;
-}
--- src/main/java/org/deri/cqels/engine/windows/Utils.java
+++
@@ -1,9 +0,0 @@
-package org.deri.cqels.engine.windows;
-
-import com.sleepycat.je.Database;
-
-public class Utils {
-	public static void deleteDB(Database db){
-		db.getEnvironment().removeDatabase(null,db.getDatabaseName());
-	}
-}
