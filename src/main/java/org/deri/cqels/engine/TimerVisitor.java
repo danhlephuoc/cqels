69b2d49e6c1820bbe8a788ff3be475dc5901971c
chanlevan <chanlevan@dbe85ffb-98f7-9858-1d73-24c5edbc5cac>
2014-04-16 18:17:14 +0000

    fix non-positive time period bug

--- src/main/java/org/deri/cqels/engine/TimerVisitor.java
+++ src/main/java/org/deri/cqels/engine/TimerVisitor.java
@@ -11,51 +11,52 @@ public class TimerVisitor implements RouterVisitor{
 		Window w = router.getWindow();
 		if (w instanceof RangeWindow) {
 			Purifier p = new Purifier((RangeWindow)w);
-			((RangeWindow) w).enableSlidePurifier(p);
+			if (((RangeWindow)w).getSlide() > 0)
+				((RangeWindow) w).enableSlidePurifier(p);
 		}
 	}
 
 	public void visit(ProjectRouter router) {
-		System.out.println("Hello ProjectRouter");
+		//System.out.println("Hello ProjectRouter");
 		
 	}
 
 	public void visit(ThroughRouter router) {
-		System.out.println("Hello ThroughRouter");
+		//System.out.println("Hello ThroughRouter");
 		
 	}
 
 	public void visit(BDBGraphPatternRouter router) {
-		System.out.println("Hello BDBGraphPatternRouter");
+		//System.out.println("Hello BDBGraphPatternRouter");
 		
 	}
 
 	public void visit(ExtendRouter router) {
-		System.out.println("Hello ExtendRouter");
+		//System.out.println("Hello ExtendRouter");
 		
 	}
 
 	public void visit(FilterExprRouter router) {
-		System.out.println("Hello FilterExpRouter");
+		//System.out.println("Hello FilterExpRouter");
 		
 	}
 
 	public void visit(ContinuousConstruct router) {
-		System.out.println("Hello ConinuousConstructRouter");
+		//System.out.println("Hello ConinuousConstructRouter");
 		
 	}
 
 	public void visit(ContinuousSelect router) {
-		System.out.println("Hello ContinousSelectRouter");
+		//System.out.println("Hello ContinousSelectRouter");
 	}
 
 	public void visit(GroupRouter router) {
-		System.out.println("Hello GroupRouter");
+		//System.out.println("Hello GroupRouter");
 		
 	}
 
 	public void visit(OpRouter router) {
-		System.out.println("Hello OpRouter");
+		//System.out.println("Hello OpRouter");
 	}
 
 }
